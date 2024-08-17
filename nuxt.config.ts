87bbe6977650ab6d1fa486a123358e244d90836a
Syed Haider Hassan <haideritx@gmail.com>
2024-08-18 00:28:30 +0500

    fix: pagespeed testings
    i just removed the css imports entirely from nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,7 +9,7 @@ export default defineNuxtConfig({
       $config: config
     }
   },
-  // css: ['~/assets/style.scss'],
+  css: ['~/assets/style.scss'],
   tailwindcss: {
     cssPath: '~/assets/css/tailwind.css',
     // cssPath: false
